ea7030be032d8ca2cde81a705ce2ff1ef612d169
Farzad Rahman <farzadrahman59@gmail.com>
2018-03-23 13:57:04 +0600

    Farzad

--- login.php
+++ login.php
@@ -1,86 +1,32 @@
 
 <!DOCTYPE html>
 <html lang="en">
-    <head>
 
-        <meta charset="utf-8" />
-<link rel="apple-touch-icon" sizes="76x76" href="assets/img/apple-icon.png">
-<link rel="icon" type="image/png" href="assets/img/favicon.png">
-<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" />
+<head>
+    <meta charset="utf-8" />
+    <link rel="apple-touch-icon" sizes="76x76" href="assets/img/apple-icon.png">
+    <link rel="icon" type="image/png" href="assets/img/favicon.png">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" />
+    <title>LLoyd Baker | Admin Panel</title>
+    <meta content='width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0, shrink-to-fit=no' name='viewport' />
+    <!--     Fonts and icons     -->
+    <link href="https://fonts.googleapis.com/css?family=Montserrat:400,700,200" rel="stylesheet" />
+    <link href="https://use.fontawesome.com/releases/v5.0.6/css/all.css" rel="stylesheet">
+    <!-- CSS Files -->
+    <link href="assets/css/bootstrap.min.css" rel="stylesheet" />
+    <link href="assets/css/now-ui-dashboard.css?v=1.0.1" rel="stylesheet" />
+    <!--    <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.0.0/css/bootstrap.css">-->
+    <link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/1.10.16/css/dataTables.bootstrap4.min.css">
 
-
-
-
-<title> Now UI Dashboard PRO  by Creative Tim</title>
-
-
-
-
-
-
-<meta content='width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0, shrink-to-fit=no' name='viewport' />
-
-
-<!-- Extra details for Live View on GitHub Pages -->
-<!-- Canonical SEO -->
-<link rel="canonical" href="https://www.creative-tim.com/product/now-ui-dashboard-pro" />
+    <!-- CSS Just for demo purpose, don't include it in your project -->
+    <link href="assets/demo/demo.css" rel="stylesheet" />
+    <script src="https://code.jquery.com/jquery-1.12.4.js"></script>
+</head>
 
 
-<!--  Social tags      -->
-<meta name="keywords" content="creative tim, html dashboard, html css dashboard, web dashboard, bootstrap 4 dashboard, bootstrap 4, css3 dashboard, bootstrap 4 admin, now ui dashboard bootstrap 4 dashboard, frontend, responsive bootstrap 4 dashboard">
-<meta name="description" content="Now UI Dashboard PRO is a beautiful Bootstrap 4 admin dashboard with a large number of components, designed to look beautiful, clean and organised. If you are looking for a tool to manage and visualise data about your business, this dashboard is the thing for you.">
+<body class=" sidebar-mini"  background="assets/img/bg14.jpg" style="background-repeat: no-repeat;">
 
-
-<!-- Schema.org markup for Google+ -->
-<meta itemprop="name" content="Now Ui Dashboard PRO by Creative Tim">
-<meta itemprop="description" content="Now UI Dashboard PRO is a beautiful Bootstrap 4 admin dashboard with a large number of components, designed to look beautiful, clean and organised. If you are looking for a tool to manage and visualise data about your business, this dashboard is the thing for you.">
-
-<meta itemprop="image" content="https://s3.amazonaws.com/creativetim_bucket/products/72/opt_nudp_thumbnail.jpg">
-
-
-<!-- Twitter Card data -->
-<meta name="twitter:card" content="product">
-<meta name="twitter:site" content="@creativetim">
-<meta name="twitter:title" content="Now Ui Dashboard PRO by Creative Tim">
-
-<meta name="twitter:description" content="Now UI Dashboard PRO is a beautiful Bootstrap 4 admin dashboard with a large number of components, designed to look beautiful, clean and organised. If you are looking for a tool to manage and visualise data about your business, this dashboard is the thing for you.">
-<meta name="twitter:creator" content="@creativetim">
-<meta name="twitter:image" content="https://s3.amazonaws.com/creativetim_bucket/products/72/opt_nudp_thumbnail.jpg">
-
-
-<!-- Open Graph data -->
-<meta property="fb:app_id" content="655968634437471">
-<meta property="og:title" content="Now Ui Dashboard PRO by Creative Tim" />
-<meta property="og:type" content="article" />
-<meta property="og:url" content="http://demos.creative-tim.com/now-ui-dashboard-pro/examples/dashboard.html" />
-<meta property="og:image" content="https://s3.amazonaws.com/creativetim_bucket/products/72/opt_nudp_thumbnail.jpg"/>
-<meta property="og:description" content="Now UI Dashboard PRO is a beautiful Bootstrap 4 admin dashboard with a large number of components, designed to look beautiful, clean and organised. If you are looking for a tool to manage and visualise data about your business, this dashboard is the thing for you." />
-<meta property="og:site_name" content="Creative Tim" />
-
-
-
-
-<!--     Fonts and icons     -->
-<link href="https://fonts.googleapis.com/css?family=Montserrat:400,700,200" rel="stylesheet" />
-<link href="https://use.fontawesome.com/releases/v5.0.6/css/all.css" rel="stylesheet">
-
-<!-- CSS Files -->
-<link href="assets/css/bootstrap.min.css" rel="stylesheet" />
-
-<link href="assets/css/now-ui-dashboard.min.css?v=1.0.1" rel="stylesheet" />
-      
-
-
-
-
-
-<!-- CSS Just for demo purpose, don't include it in your project -->
-<link href="assets/demo/demo.css" rel="stylesheet" />
-
-
-    </head>
-
-    <body class=" sidebar-mini">
+<!--<img src="assets/img/bg14.jpg">-->
         
         <!-- Navbar -->
 <nav class="navbar navbar-expand-lg navbar-transparent  navbar-absolute bg-primary fixed-top">
@@ -154,22 +100,7 @@
         <div class="wrapper wrapper-full-page ">
           
             
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-    <div class="full-page login-page section-image" filter-color="black" data-image="assets/img/bg14.jpg">
+            <div class="full-page login-page section-image" filter-color="black">
         <!--   you can change the color of the filter page using: data-color="blue | purple | green | orange | red | rose " -->
         <div class="content">
             <div class="container">
@@ -181,22 +112,7 @@
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-<div class="card card-login card-plain">
+                        <div class="card card-login card-plain">
     
     <div class="card-header ">
     <div class="logo-container">
@@ -244,17 +160,6 @@
     </div>
     
 </div>
-
-
-
-
-
-
-
-
-
-
-
 
 
 
@@ -434,274 +339,6 @@
 
   <!-- Sharrre libray -->
 <script src="assets/demo/jquery.sharrre.js"></script>
-
-<script>
-  $(document).ready(function(){
-    
-    
-    $('#facebook').sharrre({
-  share: {
-    facebook: true
-  },
-  enableHover: false,
-  enableTracking: false,
-  enableCounter: false,
-  click: function(api, options){
-    api.simulateClick();
-    api.openPopup('facebook');
-  },
-  template: '<i class="fab fa-facebook-f"></i> Facebook',
-  url: 'https://demos.creative-tim.com/now-ui-dashboard-pro/examples/dashboard.html'
-});
-
-    $('#google').sharrre({
-  share: {
-    googlePlus: true
-  },
-  enableCounter: false,
-  enableHover: false,
-  enableTracking: true,
-  click: function(api, options){
-    api.simulateClick();
-    api.openPopup('googlePlus');
-  },
-  template: '<i class="fa fa-google-plus"></i> Google',
-  url: 'https://demos.creative-tim.com/now-ui-dashboard-pro/examples/dashboard.html'
-});
-
-    $('#twitter').sharrre({
-  share: {
-    twitter: true
-  },
-  enableHover: false,
-  enableTracking: false,
-  enableCounter: false,
-  buttons: { twitter: {via: 'CreativeTim'}},
-  click: function(api, options){
-    api.simulateClick();
-    api.openPopup('twitter');
-  },
-  template: '<i class="fab fa-twitter"></i> Twitter',
-  url: 'https://demos.creative-tim.com/now-ui-dashboard-pro/examples/dashboard.html'
-});
-
-    
-    var _gaq = _gaq || [];
-_gaq.push(['_setAccount', 'UA-46172202-1']);
-_gaq.push(['_trackPageview']);
-
-(function() {
-    var ga = document.createElement('script');
-    ga.type = 'text/javascript';
-    ga.async = true;
-    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
-    var s = document.getElementsByTagName('script')[0];
-    s.parentNode.insertBefore(ga, s);
-})();
-
-    // Facebook Pixel Code Don't Delete
-!function(f,b,e,v,n,t,s){if(f.fbq)return;n=f.fbq=function(){n.callMethod?
-n.callMethod.apply(n,arguments):n.queue.push(arguments)};if(!f._fbq)f._fbq=n;
-n.push=n;n.loaded=!0;n.version='2.0';n.queue=[];t=b.createElement(e);t.async=!0;
-t.src=v;s=b.getElementsByTagName(e)[0];s.parentNode.insertBefore(t,s)}(window,
-document,'script','//connect.facebook.net/en_US/fbevents.js');
-
-try{
-  fbq('init', '111649226022273');
-  fbq('track', "PageView");
-
-}catch(err) {
-  console.log('Facebook Track Error:', err);
-}
-
-  });
-</script>
-<noscript>
-  <img height="1" width="1" style="display:none"
-src="https://www.facebook.com/tr?id=111649226022273&ev=PageView&noscript=1"
-/>
-
-</noscript>
-
-  <script>
-  $(document).ready(function(){
-    $().ready(function(){
-        $sidebar = $('.sidebar');
-        $sidebar_img_container = $sidebar.find('.sidebar-background');
-
-        $full_page = $('.full-page');
-
-        $sidebar_responsive = $('body > .navbar-collapse');
-        sidebar_mini_active = true;
-
-        window_width = $(window).width();
-
-        fixed_plugin_open = $('.sidebar .sidebar-wrapper .nav li.active a p').html();
-
-        // if( window_width > 767 && fixed_plugin_open == 'Dashboard' ){
-        //     if($('.fixed-plugin .dropdown').hasClass('show-dropdown')){
-        //         $('.fixed-plugin .dropdown').addClass('show');
-        //     }
-        //
-        // }
-
-        $('.fixed-plugin a').click(function(event){
-          // Alex if we click on switch, stop propagation of the event, so the dropdown will not be hide, otherwise we set the  section active
-            if($(this).hasClass('switch-trigger')){
-                if(event.stopPropagation){
-                    event.stopPropagation();
-                }
-                else if(window.event){
-                   window.event.cancelBubble = true;
-                }
-            }
-        });
-
-        $('.fixed-plugin .background-color span').click(function(){
-            $(this).siblings().removeClass('active');
-            $(this).addClass('active');
-
-            var new_color = $(this).data('color');
-
-            if($sidebar.length != 0){
-                $sidebar.attr('data-color',new_color);
-            }
-
-            if($full_page.length != 0){
-                $full_page.attr('filter-color',new_color);
-            }
-
-            if($sidebar_responsive.length != 0){
-                $sidebar_responsive.attr('data-color',new_color);
-            }
-        });
-
-        $('.fixed-plugin .img-holder').click(function(){
-            $full_page_background = $('.full-page-background');
-
-            $(this).parent('li').siblings().removeClass('active');
-            $(this).parent('li').addClass('active');
-
-
-            var new_image = $(this).find("img").attr('src');
-
-            if( $sidebar_img_container.length !=0 && $('.switch-sidebar-image input:checked').length != 0 ){
-                $sidebar_img_container.fadeOut('fast', function(){
-                   $sidebar_img_container.css('background-image','url("' + new_image + '")');
-                   $sidebar_img_container.fadeIn('fast');
-                });
-            }
-
-            if($full_page_background.length != 0 && $('.switch-sidebar-image input:checked').length != 0 ) {
-                var new_image_full_page = $('.fixed-plugin li.active .img-holder').find('img').data('src');
-
-                $full_page_background.fadeOut('fast', function(){
-                   $full_page_background.css('background-image','url("' + new_image_full_page + '")');
-                   $full_page_background.fadeIn('fast');
-                });
-            }
-
-            if( $('.switch-sidebar-image input:checked').length == 0 ){
-                var new_image = $('.fixed-plugin li.active .img-holder').find("img").attr('src');
-                var new_image_full_page = $('.fixed-plugin li.active .img-holder').find('img').data('src');
-
-                $sidebar_img_container.css('background-image','url("' + new_image + '")');
-                $full_page_background.css('background-image','url("' + new_image_full_page + '")');
-            }
-
-            if($sidebar_responsive.length != 0){
-                $sidebar_responsive.css('background-image','url("' + new_image + '")');
-            }
-        });
-
-        $('.switch-sidebar-image input').on("switchChange.bootstrapSwitch", function(){
-            $full_page_background = $('.full-page-background');
-
-            $input = $(this);
-
-            if($input.is(':checked')){
-                if($sidebar_img_container.length != 0){
-                    $sidebar_img_container.fadeIn('fast');
-                    $sidebar.attr('data-image','#');
-                }
-
-                if($full_page_background.length != 0){
-                    $full_page_background.fadeIn('fast');
-                    $full_page.attr('data-image','#');
-                }
-
-                background_image = true;
-            } else {
-                if($sidebar_img_container.length != 0){
-                    $sidebar.removeAttr('data-image');
-                    $sidebar_img_container.fadeOut('fast');
-                }
-
-                if($full_page_background.length != 0){
-                    $full_page.removeAttr('data-image','#');
-                    $full_page_background.fadeOut('fast');
-                }
-
-                background_image = false;
-            }
-        });
-
-        $('.switch-sidebar-mini input').on("switchChange.bootstrapSwitch", function(){
-          var $btn = $(this);
-
-          if(sidebar_mini_active == true){
-              $('body').removeClass('sidebar-mini');
-              sidebar_mini_active = false;
-              nowuiDashboard.showSidebarMessage('Sidebar mini deactivated...');
-          }else{
-              $('body').addClass('sidebar-mini');
-              sidebar_mini_active = true;
-              nowuiDashboard.showSidebarMessage('Sidebar mini activated...');
-          }
-
-          // we simulate the window Resize so the charts will get updated in realtime.
-          var simulateWindowResize = setInterval(function(){
-              window.dispatchEvent(new Event('resize'));
-          },180);
-
-          // we stop the simulation of Window Resize after the animations are completed
-          setTimeout(function(){
-              clearInterval(simulateWindowResize);
-          },1000);
-        });
-    });
-  });
-</script>
-
-
-
-
-
-
-
-
-
-
-
-
-  <script>
-  $(document).ready(function(){
-    demo.checkFullPageBackgroundImage();
-  });
-</script>
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 
