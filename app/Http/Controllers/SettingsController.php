2689973a471c76e000658b3b24e0bde5b7838f55
Farzad <farzadrahman59@gmail.com>
2019-01-07 13:34:38 +0600

    Farzad

--- app/Http/Controllers/SettingsController.php
+++ app/Http/Controllers/SettingsController.php
@@ -6,6 +6,10 @@ use Illuminate\Http\Request;
 
 class SettingsController extends Controller
 {
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
     /*Product*/
     public function product(){
         return view('settings.productCategory');
--- app/Http/Controllers/StockTransferController.php
+++ app/Http/Controllers/StockTransferController.php
@@ -6,6 +6,10 @@ use Illuminate\Http\Request;
 
 class StockTransferController extends Controller
 {
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
     public function index(){
         return view('stocktransfer.show');
     }
